f7f26666d4494ac4efe746ab38211a5838268336
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-07-31 16:59:22 -0300

    31/07/2023

--- RevisãoFPOO/App.java
+++ RevisãoFPOO/App.java
@@ -13,10 +13,13 @@ public class App {
         Cat cats[] = new Cat[10];
         Dog dogs[] = new Dog[10];
         Other others[] = new Other[10];
+        Apointments apointments[] = new Apointments[255];
         //CONTADOR
         int countCats = 0;
         int countDogs = 0;
         int countOthers = 0;
+        int countApointments = 0;
+        
         //SIGN-UP CREATION
         //System.out.println("Bem vindo ao Consultório do Dr. Frankstein");
         JOptionPane.showMessageDialog(null, "Wellcome to the Petshop");
@@ -89,13 +92,106 @@ public class App {
                     
                 }
             }
+
+            boolean apointmentOk = false;
+                String apointmentOwner = JOptionPane.showInputDialog("Informe o Nome do Dono do PET");
+                //
+                for (int i = 0; i < countCats; i++) {
+                    if (cats[i].getOwner().equals(apointmentOwner)) {
+                        JOptionPane.showMessageDialog(null, "Cadastro Encontrado");
+                        apointmentOk = true;
+                        break;
+                    }
+                }
+                for (int i = 0; i < countDogs; i++) {
+                    if (dogs[i].getOwner().equals(apointmentOwner)) {
+                        JOptionPane.showMessageDialog(null, "Cadastro Encontrado");
+                        apointmentOk = true;
+                        break;
+                    }
+                }
+                for (int i = 0; i < countOthers; i++) {
+                    if (Other[i].getOwner().equals(apointmentOwner)) {
+                        JOptionPane.showMessageDialog(null, "Cadastro Encontrado");
+                        apointmentOk = true;
+                        break;
+                    }
+                }
+                // 1º verificação ok - cadastro realizado
+                // 2º verificação
+                boolean schaduleOk = true;
+                do {
+                    String dataAgenda = JOptionPane.showInputDialog("Informe a data do Agendamento");
+                    String horaAgenda = JOptionPane.showInputDialog("Informe a hora do Agendamento");
+                    for (int i = 0; i <= countSchadulenda; i++) {
+                        if (agendamentos[i].getDataHora().equals(dataAgenda + horaAgenda)) {
+                            JOptionPane.showMessageDialog(null, "Horario já Agendado, Escolha outro");
+                        } else {
+                            schaduleOk = false;      
+                        }
+                    }
+                    JOptionPane.showMessageDialog(null,"agendamento Realizado");
+                            agendamentos[countSchadulenda] = new Agendamento();
+                            agendamentos[countSchadulenda].setData(dataAgenda);
+                            agendamentos[countSchadulenda].setHora(horaAgenda);
+                            countSchadulenda++;
+                }while (schaduleOk);
+
         }
 
     }
+
 
 
 }
 
 //NEW EXERCISE:
 /*Create the Apoitments Class and Store the Consults*/
+
+
+        /*boolean apointmentOk = true;
+        String apointmentOwner = JOptionPane.showInputDialog("Owner name: ");
+
+        for (int i = 0; i < countCats; i++ ){
+            if(cats[i].getOwner().equals(apointmentOwner)){
+                JOptionPane.showMessageDialog("Apointment found! ");
+                break;
+            }
+        }
+
+        boolean apointmentOk = true;
+        String apointmentOwner = JOptionPane.showInputDialog("Owner name: ");
+
+        for (int i = 0; i < countDogs; i++ ){
+            if(dogs[i].getOwner().equals(apointmentOwner)){
+                JOptionPane.showMessageDialog("Apointment found! ");
+                break;
+            }
+        }
+
+        boolean apointmentOk = true;
+        String apointmentOwner = JOptionPane.showInputDialog("Owner name: ");
+
+        for (int i = 0; i < countOthers; i++ ){
+            if(others[i].getOwner().equals(apointmentOwner)){
+                JOptionPane.showMessageDialog(null, "Apointment found! ");
+                break;
+            }
+        }
+        }
+
+    }
+    //VERIFY IF SIGNUP IS OK
+    //2° VERIFY
+    String dateTime = JOptionPane.showInputDialog("aaa");
+    String dateSchadule = JOptionPane.showInputDialog("aaa");
+    boolean apointmentOk = true;
+    for (int i = 0; i < countApointments; i++){
+        if(apointments[i].getDateTime().equals(dateSchadule)){
+            JOptionPane.showMessageDialog(null, "Signed-up successfully");
+        }
+        else{
+            apointmentOk = false;
+            JOptionPane.showMessageDialog(null, "Apointment was created! ");
+            apointments*/
  
