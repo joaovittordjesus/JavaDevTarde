d9a9b2c98505dc30510f1c4bb61a91a2af81a2f0
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-09-06 14:00:41 -0300

    Organizando as pastas e arquivos do repositório

--- Exercices/StringExample.java
+++
@@ -1,9 +0,0 @@
-package Exercices;
-
-public class StringExample {
-    public static void main(String[] args){
-        String greeting = "Hello World";
-        System.out.println(greeting);
-    }
-    
-}
