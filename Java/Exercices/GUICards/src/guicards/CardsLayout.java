371e1119e7d35c0f0ff4e57179144204f9aa901a
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-09-18 13:44:11 -0300

    GUI Exercises

---
+++ Java/Exercices/GUICards/src/guicards/CardsLayout.java
@@ -0,0 +1,18 @@
+package guicards;
+import javax.swing.*;
+import java.awt*;
+
+
+
+
+/**
+ *
+ * @author joaovittordjesus
+ */
+public class CardsLayout {
+    //constructor
+    public class CardsLayout{
+        
+    }
+    
+}
