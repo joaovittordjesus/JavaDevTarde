b109a7b92db99890986d2845495c69263bac7597
joaovittordjesus <joaovittordomingosdejesus@outlook.com>
2023-11-09 17:02:46 -0300

    JDBC Connection Test & SA5

--- Java/Exercices/JDBCTestMaven/src/main/java/com/mycompany/jdbctestmaven/JDBCTestMaven.java
+++ Java/Exercices/JDBCTestMaven/src/main/java/com/mycompany/jdbctestmaven/JDBCTestMaven.java
@@ -1,3 +1,5 @@
+package com.mycompany.jdbctestmaven;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
---
+++ Java/Projects/guijdbclojacarros/src/main/java/com/mycompany/Model/Carros.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Model;
+
+/**
+ *
+ * @author DevTardeA
+ */
+public class Carros {
+    //Atributos
+    private String marca;
+    private String modelo;
+    private String ano;
+    private String placa;
+    private String valor;
+
+    public Carros(String string, String string0, String string1, String string2, String string3) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
